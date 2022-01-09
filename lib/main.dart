ec3ccbc6197af089401ebcf05d40ea652891f9ac
Paige Carlson <phcarlson21@gmail.com>
2022-01-09 16:02:15 -0500

    Therapy content moved to new navigation page with therapist avatar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'dart:developer';
 import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/image.dart' as Image;
+import 'package:http/http.dart' as http;
 // import 'package:sentiment_dart/sentiment_dart.dart';
 import 'package:spotify/spotify.dart';
 
@@ -18,14 +20,13 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: "How Can You Find Relief?"),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
   final String title;
 
   @override
@@ -33,16 +34,92 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          children: [
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => SecondRoute()));
+              },
+              child: Text("Therapy Now"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+class SecondRoute extends StatelessWidget {
+  const SecondRoute({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const therapy();
+  }
+}
+
+class therapy extends StatefulWidget {
+  const therapy({Key? key}) : super(key: key);
+
+  @override
+  _therapyState createState() => _therapyState();
+}
+
+class _therapyState extends State<therapy> {
   String? name;
+  bool hateTherapist = false;
   bool busy = true;
+ CircleAvatar myTherapist = const CircleAvatar(
+   radius: 80.0,
+   backgroundImage:
+   NetworkImage('https://thispersondoesnotexist.com/image?=0'),
+   backgroundColor: Colors.transparent,
+ );
   String prompt = 'sad';
   List<Artist> artists = [];
   List<TrackSimple> tracks = [];
   List<PlaylistSimple> playlists = [];
   List<AlbumSimple> albums = [];
+  
 
-  _getRecommendations() async{
+   _getNewTherapist() {
+     // var url = Uri.parse('https://thispersondoesnotexist.com/image?=0');
+     // var response = await http.get(url);
+     //
+     //
+    setState(() {
+      myTherapist =  const CircleAvatar(
+        radius: 80.0,
+        backgroundImage:
+        NetworkImage('https://thispersondoesnotexist.com/image?=0'),
+        backgroundColor: Colors.transparent,
+      );
+    });
+
+  }
+  _getRecommendations() async {
     setState(() {
       this.busy = true;
     });
@@ -122,7 +199,7 @@ class _MyHomePageState extends State<MyHomePage> {
           //     '-------------------------------');
           setState(() {
             albums.add(item);
-          name = item.name;
+            name = item.name;
             this.busy = false;
           });
         }
@@ -130,7 +207,7 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  void changePrompt(newprompt){
+  void changePrompt(newprompt) {
     setState(() {
       this.prompt = newprompt;
       artists = [];
@@ -140,87 +217,62 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  Text getCurrentTimeText(){
+  Text getCurrentTimeText() {
     TimeOfDay now = TimeOfDay.now();
-    if(now.hour < 12){
+    if (now.hour < 12) {
       return const Text("Good morning!");
     }
-    else if (now.hour >= 12 && now.hour <= 18){
+    else if (now.hour >= 12 && now.hour <= 18) {
       return const Text("Good afternoon!");
     }
-    else{
+    else {
       return const Text("Good evening!");
     }
   }
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text("Welcome To Today's Therapy Session"),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            getCurrentTimeText(),
-            ElevatedButton(
-              child: Text('Feeling sad?'),
-              onPressed: (){changePrompt("sad"); _getRecommendations();},
-            ),
-            ElevatedButton(
-              child: Text('Feeling happy?'),
-              onPressed: (){changePrompt("happy");_getRecommendations();},
-            ),
-            ElevatedButton(
-              child: Text('Feeling angry?'),
-              onPressed: (){changePrompt("angry");_getRecommendations();},
-            ),
-            busy ? CircularProgressIndicator() : Text(name ?? 'unknown'),
-            // busy ? CircularProgressIndicator() : Text(artist?.name ?? 'unknown'),
-          ],
-        ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              myTherapist,
+              getCurrentTimeText(),
+          ElevatedButton(
+            child: Text('Hate me?'),
+            onPressed:_getNewTherapist
+          ),
+              ElevatedButton(
+                child: Text('Feeling sad?'),
+                onPressed: () {
+                  changePrompt("sad");
+                  _getRecommendations();
+                },
+              ),
+              ElevatedButton(
+                child: Text('Feeling happy?'),
+                onPressed: () {
+                  changePrompt("happy");
+                  _getRecommendations();
+                },
+              ),
+              ElevatedButton(
+                child: Text('Feeling angry?'),
+                onPressed: () {
+                  changePrompt("angry");
+                  _getRecommendations();
+                },
+
+              ),
+              busy ? CircularProgressIndicator() : Text(name ?? 'unknown'),
+              // busy ? CircularProgressIndicator() : Text(artist?.name ?? 'unknown'),
+            ],
+          )
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
